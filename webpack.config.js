615146c7097748bdff3302cce2c2dd395e7e579d
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2016-12-31 10:19:23 +0900

    webpackを使用してJSXをJavaScriptに変換する

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
