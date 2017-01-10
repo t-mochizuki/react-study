66ce56338d6fa8792ab66929d648630c2a7e812b
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2017-01-10 20:29:53 +0900

    stateの監視

--- src/components/radio_input.jsx
+++ src/components/radio_input.jsx
@@ -6,20 +6,33 @@ export default class RadioInput extends Component {
     name: React.PropTypes.string.isRequired,
     label: React.PropTypes.string.isRequired,
     value: React.PropTypes.string.isRequired,
-    checked: React.PropTypes.bool
+    checked: React.PropTypes.bool,
+    onChange: React.PropTypes.func.isRequired
   }
+  handleChanged(e) {}
   defaultProps() {
     return {
       id: null,
       checked: false
     };
   }
+  initialState() {
+    return {
+      checked: !!this.props.checked,
+      id: this.props.id ? this.props.id : uniqueId('radio-')
+    };
+  }
   render() {
     return (
       <div className="radio">
-        <label>
-          <input type="radio" />
-          ラベルの文字列
+        <label htmlFor="{this.state.id}">
+          <input type="radio"
+            name="{this.props.name}"
+            id="{this.state.id}"
+            value="{this.props.value}"
+            checked="{this.state.checked}"
+            onChange={this.handleChanged} />
+          {this.props.label}
         </label>
       </div>
     );
